0920d1c17c8838f18f9902eedc8c3b91c56999d7
jananga <kavinduwijewardhana1999@gmail.com>
2024-08-01 17:01:32 +0530

    add table and form part

--- app/page.js
+++ app/page.js
@@ -1,8 +1,40 @@
+import Todo from "@/Components/Todo";
 
 export default function Home() {
   return (
     <>
-    
+      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input type="text" name="title" placeholder="Enter Title"  className="px-3 py-2 border-2 w-full"/>
+        <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
+        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add Todo</button>
+      </form>
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+    <table className="w-full text-sm text-left text-gray-900">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Id
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <Todo/>
+        </tbody>
+    </table>
+</div>
+
     </>
   );
 }

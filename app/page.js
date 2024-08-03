7c96a4a85772f961d8d1576fd01f5b5401657bd2
jananga <kavinduwijewardhana1999@gmail.com>
2024-08-03 20:39:49 +0530

    update app/page.js

--- app/page.js
+++ app/page.js
@@ -15,9 +15,13 @@ export default function Home() {
         setFormData(form => ({...form, [name]:value}));
         console.log(formData);
     }
+
+    const onSubmitHandler = async () => {
+        e.preventDefaault();
+    }
   return (
     <>
-      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+      <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
         <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title"  className="px-3 py-2 border-2 w-full"/>
         <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
         <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add Todo</button>

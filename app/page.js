a779f5717fb83925b3a4913ef9a7537f6ee89232
Jananga Wijewardhana <kavinduwijewardhana1999@gmail.com>
2024-08-05 22:36:21 +0530

    add- api

--- app/page.js
+++ app/page.js
@@ -25,13 +25,13 @@ export default function Home() {
 
             toast.success('Success');
         } catch (error) {
-            toast.error('An error occurred');
+            toast.error('Error');
         }
     };
 
   return (
     <>
-        <ToastContainer/>
+        <ToastContainer theme="dark"/>
         <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
             <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title"  className="px-3 py-2 border-2 w-full"/>
             <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>

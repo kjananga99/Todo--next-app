89c02ba7fbcc66e930b2bcdfb939af1594f87e20
Jananga Wijewardhana <kavinduwijewardhana1999@gmail.com>
2024-08-06 14:08:37 +0530

    connect - GET and POST

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,29 @@
 
+import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/TodoModel";
 import { NextResponse } from "next/server";
 
-export async function GET(request){
-    return NextResponse.json({msg:"get method hit"})
+const LoadDB = async () => {
+    await ConnectDB();
+};
+
+LoadDB();
+
+export async function GET(request) {
+    await LoadDB();
+    return NextResponse.json({msg:"get method hit"});
+}
+
+export async function POST(request) {
+
+    await LoadDB();
+
+    const {title, description} = await request.json();
+
+    await TodoModel.create({
+        title,
+        description
+    })
+
+    return NextResponse.json({ msg: "Todo Created" });
 }
